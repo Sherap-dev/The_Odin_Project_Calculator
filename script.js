5f4c9804fad7b99c5c876bef6b9e82ecb95ea007
sherap <3dsherap@gmail.com>
2025-12-01 20:28:27 +0530

    finish the basic calculation function

--- script.js
+++ script.js
@@ -1,46 +1,90 @@
 const buttons = document.querySelectorAll(".btn");
+const evalBtn = document.querySelector("#equal")
 const display = document.querySelector("#display");
 
-let inputNum = "";
-let firstNum = "";
-let secondNum = "";
+let firstOperand = "";
+let secondOperand = "";
+let secondOperandready = false;
 let operator = "";
+let result = ""
 
+function add(firstOperand,secondOperand){
+    result = firstOperand + secondOperand;
+    display.value = result
+}
+
+function substract(firstOperand,secondOperand){
+    result = firstOperand - secondOperand;
+    display.value = result
+}
+
+function multiply(firstOperand,secondOperand){
+    result = firstOperand * secondOperand;
+    display.value = result
+}
+
+function divide(firstOperand,secondOperand){
+    if(secondOperand === 0 || firstOperand === 0){
+        let message = "error"
+        display.value = message
+    }
+    result = firstOperand / secondOperand;
+    display.value = result
+}
+
+
+evalBtn.addEventListener("click", () => {
+
+    switch(operator){
+        case "+" :
+            add(firstOperand,secondOperand);;
+            break;
+        case "-" :
+            substract(firstOperand,secondOperand);
+            break;
+        case "*" :
+            multiply(firstOperand,secondOperand);
+            break;
+        case "/" :
+            divide(firstOperand,secondOperand);
+    }
+
+})
+
+substract(firstOperand,secondOperand)
 
 
 buttons.forEach(btn =>{
     btn.addEventListener("click",(e) => {
         let input = e.target.textContent;
 
-        if(input === "." && firstNum.includes(".")){
-            return;
+        if(input === "." && display.value.includes(".")){
+            return
         }
 
-        if(!isNaN(input) || input === "."){
-            inputNum += input;
-            display.value = inputNum;
-         
-        }else if (input === "-" || input === "+" || input === "/" || input === "*"){
-            operator = input;
-            firstNum = display.value
-            display.value = ""
-            secondNum = firstNum
-            console.log(secondNum)
-            display.value = operator;
-        }else if( input === "C"){
-            operator = "";
-            firstNum = "";
-            secondNum = "";
-            inputNum = "";
-            display.value = "";
+       if(!isNaN(input) || input === "."){
+        display.value += input;
+
+        if(!secondOperandready){
+            firstOperand = parseFloat(display.value);
+        }else { secondOperand = parseFloat(display.value);
+            console.log(firstOperand)
+            console.log(secondOperand)
         }
-        
-    //  if(operator.includes("+") || operator.includes("/") || operator.includes("*") || operator.includes("-")){
-    //     secondNum = inputNum;
-    //     console.log(`im secoond numbners${typeof secondNum}`)
-    //  }
 
-
-
+       }
+       else if(input === "-" || input === "+" || input === "/" || input === "*"){
+        operator = input;
+        display.value = "";
+        secondOperandready = true;
+       }
+       else if(input === "C"){
+        display.value = "";
+        operator = ""
+        firstOperand = "";
+        secondOperand = ""
+        secondOperandready = false
+       }
     })
 })
+
